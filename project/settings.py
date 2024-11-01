30fed117457c98dd48cb4eaf610094baeb2fb3ed
fao-spec <arowofavour20@gmail.com>
2024-10-31 19:19:55 -0700

    Configured secret key from environment variable

--- project/settings.py
+++ project/settings.py
@@ -21,7 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qdill@=(xh)^fbtvxpzwnb2256^+=x)i=a5rl9yyxor!ll71p*'
+from decouple import config
+
+# Retrieve the secret key from the .env file
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
